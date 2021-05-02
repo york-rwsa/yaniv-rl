f4bfbdba1a76fa2f16046193e9633a70fe0987af
Rory Anderson <rory@jragon.co.uk>
2021-05-02 15:41:20 +0100

    fix rlcard

--- yaniv_rl/game/judger.py
+++
@@ -1,8 +0,0 @@
-from rlcard.core import Card
-from typing import List
-
-
-class YanivJudger(object):
-    def __init__(self, np_random):
-        """Initialize a yaniv judger class"""
-        self.np_random = np_random
