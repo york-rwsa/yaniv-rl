b43d7c818e977eff6f3c48d4839e4e805e7bf2b0
Rory Anderson <rory@jragon.co.uk>
2021-05-06 22:48:47 +0100

    add novice rule agent

--- yaniv_rl/utils/rllib/step_functions.py
+++ yaniv_rl/utils/rllib/step_functions.py
@@ -1,11 +1,29 @@
-from yaniv_rl.models.yaniv_rule_models import YanivIntermediateRuleAgent
+from yaniv_rl.models.yaniv_rule_models import YanivIntermediateRuleAgent, YanivNoviceRuleAgent
 from yaniv_rl.envs.rllib_multiagent_yaniv import YanivEnv
 
-def intermediate_rule_step(env: YanivEnv):
-    state = env.game.get_state(env.current_player)
+def extract_rlcard_state(game, player):
+    state = game.get_state(player)
     extracted_state = {}
     extracted_state["raw_obs"] = state
     extracted_state["raw_legal_actions"] = state["legal_actions"]
 
-    action = YanivIntermediateRuleAgent._step_multi(extracted_state)
+    return extracted_state
+
+def intermediate_rule_step(env: YanivEnv):
+    if env.single_step:
+        raise NotImplementedError
+    
+    state = extract_rlcard_state(env.game, env.current_player)
+    action = YanivIntermediateRuleAgent._step_multi(state)
     env.game.step(action)
+
+def novice_rule_step(env: YanivEnv):
+    state = extract_rlcard_state(env.game, env.current_player)
+
+    if env.single_step:
+        action = YanivNoviceRuleAgent._step_single(state)
+    else:
+        action = YanivNoviceRuleAgent._step_multi(state)
+        
+    env.game.step(action)
+    
